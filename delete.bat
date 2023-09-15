1622840762889f2d48355195423eb900e3719bb0
alexsuf <aleksey.zadonsky@gmail.com>
2023-09-15 11:53:29 +0300

    First commit

---
+++ delete.bat
@@ -0,0 +1,9 @@
+rd /s /q connect_config
+rd /s /q connect_data
+rd /s /q connect_logs
+rd /s /q kafka_data
+rd /s /q kafka_secrets
+rd /s /q zookeeper_data
+rd /s /q zookeeper_log
+rd /s /q zookeeper_secrets
+timeout /t 5
